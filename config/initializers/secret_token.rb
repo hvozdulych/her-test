a74b6fe24359cbcfee47e1707910a4ce05aa71f0
hvozdulych <hvozdulych@gmail.com>
2014-12-08 01:58:53 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestDepp::Application.config.secret_token = '87321cba4ad7ce9c0a759ac4ed7f13ad65bad26f17358e3886de8e245a314643bf7906a178e253b9062049274a3b932cd6fb1c6d4c6de774096bdec438baf752'
